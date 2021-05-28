c36ab4bef8d4b8385d2f2896d6788b72089543d4
Thangam sakthivel <43291216+Thangamsakthivel@users.noreply.github.com>
2021-05-28 20:03:08 +0530

    Update ResourceAllocator.py

--- ResourceAllocator.py
+++ ResourceAllocator.py
@@ -1,3 +1,6 @@
+region = ["uswest","useast"]
+r = int(input("Select region\n1.uswest\n2.useast\nEnter your choice: "))-1
+
 class calc:
     types = {
         1: "large",
@@ -7,7 +10,6 @@ class calc:
         16: "8xlarge",
         32: "10xlarge"
     }
-    print(types[1])
     useast = {
         "large": 0.12,
         "xlarge": 0.23,
@@ -23,25 +25,43 @@ class calc:
         "8xlarge": 1.3,
         "10xlarge":2.97
     }
-    def getCost(instances, hours, cpus, price):
-        print(calc.uswest["large"])
+
+    def getCost(hours, serverCount, serverType):
+        
 
     def byCpu(hours,cpus):
-        binary = []
-        while cpus != 0:
-            binary.append(cpus%2)
-            cpus = cpus/2
+        if(region[r] == "useast"):
+            c = [32, 16, 8, 4, 2, 1]
+            p = [0, 0, 0, 0, 0, 0]
+        elif(region[r] == "uswest"):
+            c = [32, 16, 8, 4, 1]
+            p = [0, 0, 0, 0, 0]
+        s = cpus
+        for i in range(len(c)):
+            if(s == 0):
+                break
+            elif(c[i] <= s):
+                p[i] = s//c[i]
+                s = s%c[i]
+        serverType = []
+        serverCount = []
+        for i in range(len(p)):
+            if(p[i] != 0):
+                serverType.append(calc.types[c[i]])
+                serverCount.append(p[i])
+        calc.getCost(hours, serverCount, serverType)
 
-print("Enter your Choice")
+
+print("Select type")
 print("1. Minimum N CPUs for H hours")
 print("2. Maximum price they are willing to pay for H hours")
-print("3. Combination of both")
-
-option = int(input(">>>"))
+print("3. Combination of both 1 and 2")
+option = int(input("Enter your Choice: "))
 
 if(option == 1):
     hours = int(input("Enter hours required: "))
     cpus = int(input("Enter the number of CPU required: "))
+    calc.byCpu(hours,cpus)
 elif(option == 2):
     hours = int(input("Enter hours required: "))
     price = float(input("Enter how much price willing to pay: "))
@@ -49,5 +69,5 @@ elif(option == 3):
     hours = int(input("Enter hours required: "))
     cpus = int(input("Enter the number of CPU required: "))
     price = float(input("Enter how much price willing to pay: "))
-
-c = calc
+    
+    
