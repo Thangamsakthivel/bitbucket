ceb935cfb83dd77737ab1f0a2d62ab9ea580785f
Thangam sakthivel <43291216+Thangamsakthivel@users.noreply.github.com>
2021-05-30 19:01:20 +0530

    Update ResourceAllocator.py

--- ResourceAllocator.py
+++ ResourceAllocator.py
@@ -1,59 +1,70 @@
+instances={
+    0 : {
+        0.12: "large",
+        0.23: "xlarge",
+        0.45: "2xlarge",
+        0.774: "4xlarge",
+        1.4: "8xlarge",
+        2.82: "10xlarge"
+    },
+    1 : {
+        0.14: "large",
+        0.413: "2xlarge",
+        0.89: "4xlarge",
+        1.30: "8xlarge",
+        2.97: "10xlarge"
+    }
+}
 class calc:
     types = {
-        1: "large",
-        2: "xlarge",
-        4: "2xlarge",
-        8: "4xlarge",
-        16: "8xlarge",
-        32: "10xlarge"
+        0 : {
+            1: "large",  
+            2: "xlarge",
+            4: "2xlarge",
+            8: "4xlarge",
+            16: "8xlarge",
+            32: "10xlarge"
+        },
+        1 : {
+            1: "large",
+            4: "2xlarge",
+            8: "4xlarge",
+            16: "8xlarge",
+            32: "10xlarge"
+        }
     }
 
-    def byCpu(hours, cpus):
-        c = [[32, 16, 8, 4, 2, 1], [32, 16, 8, 4, 1]]
-        p = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-        serverType = []
-        serverCount = []
+    def by(value, option, types):  #To calculate the requirment of the server by using CPU count 
+        if(option == "cpus"):
+            c = [[32, 16, 8, 4, 2, 1], [32, 16, 8, 4, 1]]
+        elif(option == "price"):
+            c = [[2.82, 1.4, 0.774, 0.45, 0.23, 0.12], [2.97, 1.3, 0.89, 0.413, 0.14]]
+        serverType, serverCount = [], []
         for i in range(2):
-            s = cpus
+            s = value
+            st = []
+            sc = []
             for j in range(len(c[i])):
                 if(s == 0):
                     break
                 elif(c[i][j] <= s):
-                    p[i][j] = s//c[i][j]
-                    s = s%c[i][j]
-            st = []
-            sc = []
-            for j in range(len(p[i])):
-                if(p[i][j] != 0):
-                    st.append(calc.types[c[i][j]])
-                    sc.append(p[i][j])
+                    st.append(types[i][c[i][j]])
+                    sc.append(s//c[i][j])
+                    s = s%c[i][j]      
             serverType.append(st)
             serverCount.append(sc)
-        return serverCount,serverType
+        return serverCount, serverType
 
-    def getCost(instances, hours, cpus, price):
-        if(price == 0):
-            serverCount,serverType=calc.byCpu(hours, cpus)
-            print(serverCount,serverType)
-
-
-instances={
-    "useast" : {
-        "large": 0.12,
-        "xlarge": 0.23,
-        "2xlarge": 0.45,
-        "4xlarge": 0.774,
-        "8xlarge": 1.4,
-        "10xlarge": 2.82
-    },
-    "uswest" : {
-        "large": 0.14,
-        "2xlarge": 0.413,
-        "4xlarge": 0.89,
-        "8xlarge": 1.3,
-        "10xlarge":2.97
-    }
-}
+    def getCost(hours, cpus, price):
+        serverCount, serverType = [], []
+        if(price == 0 and cpus != 0):
+            serverCount, serverType=calc.by(cpus, "cpus", calc.types)
+            print(serverCount, serverType)
+        elif(cpus == 0 and price != 0):
+            serverCount, serverType=calc.by(price/hours, "price", instances)
+            print(serverCount, serverType)
+        else:
+            pass           
 
 print("Select type")
 print("1. Minimum N CPUs for H hours")
@@ -64,11 +75,13 @@ option = int(input("Enter your Choice: "))
 if(option == 1):
     hours = int(input("Enter hours required: "))
     cpus = int(input("Enter the number of CPU required: "))
-    calc.getCost(instances, hours, cpus, 0)
+    calc.getCost(hours, cpus, 0)
 elif(option == 2):
     hours = int(input("Enter hours required: "))
     price = float(input("Enter how much price willing to pay: "))
+    calc.getCost(hours, 0, price)
 elif(option == 3):
     hours = int(input("Enter hours required: "))
     cpus = int(input("Enter the number of CPU required: "))
     price = float(input("Enter how much price willing to pay: "))
+
