eccb2dc5e53aee882a09fe0bf1e5b4e0b3a36a19
Thangam sakthivel <43291216+Thangamsakthivel@users.noreply.github.com>
2021-05-31 09:37:34 +0530

    Update ResourceAllocator.py

--- ResourceAllocator.py
+++ ResourceAllocator.py
@@ -1,4 +1,3 @@
-import math
 instances={
     0 : {
         0.12: "large",
@@ -35,7 +34,12 @@ class calc:
         }
     }
 
-    def by(value, hours, types, option):  #To calculate the requirment of the server by using CPU count 
+    def getKey(i,val):
+        for key, value in instances[i].items():
+             if val == value:
+                 return key
+
+    def bySingleValue(value, hours, types, option):  #To calculate the requirment of the server by using CPU count/Price 
         if(option == "cpus"):
             c = [[32, 16, 8, 4, 2, 1], [32, 16, 8, 4, 1]]
         elif(option == "price"):
@@ -52,37 +56,56 @@ class calc:
                     if(s//(c[i][j]*hours) >= 1.0):
                         sc.append(int((s//(c[i][j]*hours))))
                         st.append(types[i][c[i][j]])
-                    s = s%(c[i][j]*hours)      
+                        s = s%(c[i][j]*hours)      
             serverType.append(st)
             serverCount.append(sc)
         return serverCount, serverType
 
-    def getKey(i,val):
-        for key, value in instances[i].items():
-             if val == value:
-                 return key
+    def byCombinationOfBoth(hours, Cpus, Price, types):
+        serverOption = [[32, 16, 8, 4, 2, 1], [32, 16, 8, 4, 1]]
+        priceOption = [[2.82, 1.4, 0.774, 0.45, 0.23, 0.12], [2.97, 1.3, 0.89, 0.413, 0.14]]
+        serverType, serverCount = [], []
+        for i in range(2):
+            cpus = Cpus
+            price = Price
+            st = []
+            sc = []
+            for j in range(len(serverOption[i])):
+                if(cpus == 0):
+                    break
+                elif(serverOption[i][j] <= cpus and priceOption[i][j] <= price):
+                    if(((price//(priceOption[i][j]*hours) >= 1.0) and cpus//serverOption[i][j])):
+                        sc.append(int((price//(priceOption[i][j]*hours))))
+                        st.append(instances[i][priceOption[i][j]])
+                        price = price%(priceOption[i][j]*hours)
+            serverType.append(st)
+            serverCount.append(sc)
+        return serverCount, serverType
+
 
     def getCost(hours, cpus, price):
         serverCount, serverType = [], []
         if(price == 0 and cpus != 0):
-            serverCount, serverType=calc.by(cpus, 1, calc.types, "cpus")
+            serverCount, serverType = calc.bySingleValue(cpus, 1, calc.types, "cpus")
         elif(cpus == 0 and price != 0):
-            serverCount, serverType=calc.by(price, hours, instances, "price")
+            serverCount, serverType = calc.bySingleValue(price, hours, instances, "price")
         else: 
-            start()
+            serverCount, serverType = calc.byCombinationOfBoth(hours, cpus, price, calc.types)
         cost = []
+        server = [[],[]]
         for i in range(2):
             c=0
             for j in range(len(serverCount[i])):
                 costPerServer = calc.getKey(i,serverType[i][j])
                 c += costPerServer*serverCount[i][j]
+                server[i].append((serverType[i][j],serverCount[i][j]))
             cost.append(round(c*hours,2))
         region = ["us-east", "us-west"]
-        result = {0:{},1:{}}
+        result = [{},{}]
         for i in range(2):
             result[i].update({"region":region[i]})
             result[i].update({"totol-cost":"$"+str(cost[i])})
-            result[i].update({"server":[serverType[i],serverCount[i]]})
+            result[i].update({"server":server[i]})
         print(result)
         
 
@@ -102,8 +125,6 @@ def start():
         price = float(input("Enter how much price willing to pay: "))
         calc.getCost(hours, 0, price)
     elif(option == 3):
-        print("Sorry! this feature have not completed yet")
-        exit(0)
         hours = int(input("Enter hours required: "))
         cpus = int(input("Enter the number of CPU required: "))
         price = float(input("Enter how much price willing to pay: "))
